cfcb92270e376c7bc8ee36d2d916b10b6c6c1cf1
Suzanne Wilson <suzanne.wight.wilson@gmail.com>
2020-08-08 14:37:07 -0700

    circle fill is still black

--- static/js/logic.js
+++ static/js/logic.js
@@ -12,13 +12,7 @@ let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 //     ],
 //     zoom: 4
 //   });
-//  Add a marker to the map for Los Angeles, California.
-L.circleMarker([34.0522, -118.2437], {
-	radius: 300,
-	color:"black",
-	fillcolor:'#ffffa1',
-    fillOpacity: 0.5
- }).addTo(map);
+
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -29,4 +23,10 @@ attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStree
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
-
+//  Add a marker to the map for Los Angeles, California.
+L.circleMarker([34.0522, -118.2437], {
+	radius: 300,
+	color:"black",
+	fillcolor:"canary",
+    fillOpacity: 0.5
+ }).addTo(map);
