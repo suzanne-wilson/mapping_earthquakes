7574803498ff762c9f2e1cfb443b70453d9ea591
Suzanne Wilson <suzanne.wight.wilson@gmail.com>
2020-08-08 20:57:02 -0700

    major airports

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,7 +2,8 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// let map = L.map('mapid').setView([40.7, -94.5], 4);
+
 // Create the map object with a center and zoom level.
 // Create the map object with center at the San Francisco airport.
 // let map = L.map('mapid').setView([37.6213, -122.3790], 14);
@@ -15,10 +16,57 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //     ],
 //     zoom: 4
 //   });
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
 
-// We create the tile layer that will be the background of our map. streets-v11 dark-v10 satellite-streets-v11
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// Grabbing our GeoJSON data.
+// L.geoJson(sanFranAirport, {
+//     // We turn each feature into a marker on the map.
+//     pointToLayer: function(feature, latlng) {
+//       console.log(feature);
+// 	  return L.marker(latlng)
+// 	  .bindPopup("<h2>" + feature.properties.name+ "</h2> <hr> <h3>" + feature.properties.city + ", " + feature.properties.country + "</h3>");
+//     }
+
+//   }).addTo(map);
+
+//   L.geoJson(sanFranAirport, {
+//     onEachFeature: function(feature, layer) {
+// 	  console.log(feature);
+//       layer.bindPopup();
+//      }
+// }).addTo(map);
+L.geoJson(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    pointToLayer: function(feature, latlng) {
+      console.log(feature);
+	  return L.marker(latlng)
+	  .bindPopup("<h3>" + "Airport code: " + feature.properties.faa + "</h3> <hr> <h3>Airport name: " + feature.properties.name + "</h3>");
+    }
+
+  }).addTo(map);
+// We create the tile layer that will be the background of our map. streets-v11 dark-v10 satellite-streets-v11 navigation-preview-night-v2
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
@@ -40,27 +88,27 @@ let cityData = cities;
 // 		fillcolor:"orange",
 // 		fillOpacity: 0.2
 // 	})
-// 	.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+ 	// .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
 //   .addTo(map);
 // });
 
 // Coordinates for each point to be used in the line.
 // Coordinates for each point to be used in the polyline.
-let line = [
-	// SFO to JFK with two stops, Austin-Bergstrom (AUS) and Toronto Pearson (YYZ)
-	[37.6213, -122.3790],
-	[30.1975,  -97.6664],
-	[43.6532,  -79.3832],
-	[40.6413,  -73.7781]
-  ];
+// let line = [
+// 	// SFO to JFK with two stops, Austin-Bergstrom (AUS) and Toronto Pearson (YYZ)
+// 	[37.6213, -122.3790],
+// 	[30.1975,  -97.6664],
+// 	[43.6532,  -79.3832],
+// 	[40.6413,  -73.7781]
+//   ];
 
-// Create a polyline using the line coordinates and make the line black.
-L.polyline(line, {
-	color: "blue",
-	weight: '4', 
-	strokeOpacity: 0.2,
-	dashArray: '5, 10'
- }).addTo(map);
+// // Create a polyline using the line coordinates and make the line black.
+// L.polyline(line, {
+// 	color: "blue",
+// 	weight: '4', 
+// 	strokeOpacity: 0.2,
+// 	dashArray: '5, 10'
+//  }).addTo(map);
 // L.circleMarker([34.0522, -118.2437], {
 // 	radius: 300,
 // 	color:"black",
