3a191b11fbe6118d2f8d4d6682b9df25ce3b9ddb
Suzanne Wilson <suzanne.wight.wilson@gmail.com>
2020-08-09 16:02:38 -0700

    update logic

--- static/js/logic.js
+++ static/js/logic.js
@@ -68,19 +68,21 @@ console.log("linestrings");
 
 // Create the map object with center and zoom level.
 // let map = L.map('mapid').setView([30, 30], 2);
-  
-// Create the map object with center, zoom level and default layer.
-let map = L.map('mapid', {
-	center: [30, 30],
-	zoom: 2,
-	layers: [streets]
-});
+ 
 // We create the tile layer that will be the background of our map. streets-v11 dark-v10 satellite-streets-v11 navigation-preview-night-v2
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
 });
+ 
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+	center: [30, 30],
+	zoom: 2,
+	layers: [streets]
+});
+
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
