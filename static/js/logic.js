229b40a0ad3a60d6e0714d5937af5d1d54b67504
Suzanne Wilson <suzanne.wight.wilson@gmail.com>
2020-08-08 21:22:44 -0700

    url for github file

--- static/js/logic.js
+++ static/js/logic.js
@@ -17,8 +17,9 @@ console.log("working");
 //     zoom: 4
 //   });
 // Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
-
+// let map = L.map('mapid').setView([37.5, -122.5], 10);
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
 // Add GeoJSON data.
 let sanFranAirport =
 {"type":"FeatureCollection","features":[{
@@ -56,28 +57,38 @@ let sanFranAirport =
 //       layer.bindPopup();
 //      }
 // }).addTo(map);
-L.geoJson(sanFranAirport, {
-    // We turn each feature into a marker on the map.
-    pointToLayer: function(feature, latlng) {
-      console.log(feature);
-	  return L.marker(latlng)
-	  .bindPopup("<h3>" + "Airport code: " + feature.properties.faa + "</h3> <hr> <h3>Airport name: " + feature.properties.name + "</h3>");
-    }
+// L.geoJson(sanFranAirport, {
+//     // We turn each feature into a marker on the map.
+//     pointToLayer: function(feature, latlng) {
+//       console.log(feature);
+// 	  return L.marker(latlng)
+// 	  .bindPopup("<h3>" + "Airport code: " + feature.properties.faa + "</h3> <hr> <h3>Airport name: " + feature.properties.name + "</h3>");
+//     }
 
-  }).addTo(map);
+//   }).addTo(map);
 // We create the tile layer that will be the background of our map. streets-v11 dark-v10 satellite-streets-v11 navigation-preview-night-v2
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
 });
-
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://github.com/suzanne-wilson/mapping_earthquakes/tree/Mapping_GeoJSON_Points/static/js/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data).addTo(map);
+});
+
 //  Add a marker to the map for Los Angeles, California.
 // An array containing each city's location, state, and population.
 // Get data from cities.js
-let cityData = cities;
+// let cityData = cities;
 
 // Loop through the cities array and create one marker for each city.
 // cityData.forEach(function(city) {
