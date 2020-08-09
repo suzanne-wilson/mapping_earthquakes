8f059f38c6f26f980b8dfc2904ad967e5c394ce6
Suzanne Wilson <suzanne.wight.wilson@gmail.com>
2020-08-09 13:13:12 -0700

    try again

--- static/js/logic.js
+++ static/js/logic.js
@@ -68,7 +68,7 @@ console.log("working");
 
 // Create the map object with center and zoom level.
 // let map = L.map('mapid').setView([30, 30], 2);
-
+  
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
 	center: [30, 30],
