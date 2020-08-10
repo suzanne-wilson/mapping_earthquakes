c5c6c98ecd8af4bfdee53548904dd1e1817cc522
Suzanne Wilson <suzanne.wight.wilson@gmail.com>
2020-08-09 17:40:48 -0700

    it works

--- static/js/logic.js
+++ static/js/logic.js
@@ -75,16 +75,7 @@ attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStree
 	maxZoom: 18,
 	accessToken: API_KEY
 });
- 
-// Create the map object with center, zoom level and default layer.
-let map = L.map('mapid', {
-	center: [30, 30],
-	zoom: 2,
-	layers: [streets]
-});
 
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
 
 // We create the dark view tile layer that will be an option for our map.
 let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -99,17 +90,40 @@ let baseMaps = {
 	Dark: dark
   };
 
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+	center: [30, 30],
+	zoom: 2,
+	layers: [streets]
+});
+ 
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-let airportData = "https://raw.githubusercontent.com/suzanne-wilson/mapping_earthquakes/master/majorAirports.json";
 // let airportData = majorAirports;
+const airportData = "https://raw.githubusercontent.com/suzanne-wilson/mapping_earthquakes/Mapping_GeoJSON_Linestrings/majorAirports.json";
+
+// // Grabbing our GeoJSON data.
+// d3.json(airportData).then(function(data) {
+//     console.log(data);
+//   // Creating a GeoJSON layer with the retrieved data.
+//   L.geoJson(data).addTo(map);
+// });
+
 // Grabbing our GeoJSON data.
-/// Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
+  L.geoJson(data, {
+	onEachFeature: function(feature, layer) {
+	  console.log(feature);
+	  layer.bindPopup("<h3>" + "Airport code: " + feature.properties.faa + "</h3> <hr> <h3>Airport name: " + feature.properties.name + "</h3>");
+	 }
+})
+.addTo(map);
 });
 
 //  Add a marker to the map for Los Angeles, California.
